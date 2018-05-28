59d14d084a135331e3c5e673281b4342ed567528
Andrew Zigler <andrewzigler@gmail.com>
2018-05-28 15:14:41 -0700

    feat: Add alt text to restuarant images
    - create helper function to retrieve alt text

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -154,6 +154,13 @@ class DBHelper {
   }
 
   /**
+   * Restaurant image alt text.
+   */
+  static imageAltForRestaurant(restaurant) {
+    return restaurant.alt;
+  }
+
+  /**
    * Map marker for a restaurant.
    */
   static mapMarkerForRestaurant(restaurant, map) {
