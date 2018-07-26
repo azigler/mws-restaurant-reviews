480dd3fde94722a62a57d2a184e8334317e1934b
Andrew Zigler <andrewzigler@gmail.com>
2018-07-25 22:04:27 -0700

    chore: Comment out debugging console logs

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -178,7 +178,7 @@ class DBHelper {
       const tx = db.transaction('reviews-stash', 'readwrite');
       const store = tx.objectStore('reviews-stash');
 
-      console.log(`staging ${JSON.stringify(data)} for posting...`);
+      //console.log(`staging ${JSON.stringify(data)} for posting...`);
       store.put(data).catch(err => console.error(err));
       return tx.complete;
     });
@@ -192,7 +192,7 @@ class DBHelper {
       if (reviews.length) {
         reviews.forEach(function(item){
           let json = JSON.stringify(item);
-          console.log(`submitting ${json} to server...`);
+          // console.log(`submitting ${json} to server...`);
           fetch(DBHelper.REVIEWS_URL, {
             method: 'post',
             body: JSON.stringify(item)
@@ -205,7 +205,7 @@ class DBHelper {
           .catch(err => console.error(err));
         });
       } else {
-        console.log('nothing to sync!');
+        // console.log('nothing to sync!');
       }
     });
   }
@@ -246,7 +246,7 @@ class DBHelper {
    * Toggle restaurant id favorite on server.
    */
   static updateFavoriteOnServer(id, fav) {
-    console.log(`toggling ${id} to ${fav}`);
+    // console.log(`toggling ${id} to ${fav}`);
     fetch(`http://localhost:1337/restaurants/${id}/?is_favorite=${fav}`, {
       method: 'post',
     }).catch(err => console.error(err));
@@ -456,9 +456,9 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', function () {
     navigator.serviceWorker.register('sw.js')
       .then(function(registration) {
-        console.log(`Service worker registered successfully with scope: ${registration.scope}`);
+        // console.log(`Service worker registered successfully with scope: ${registration.scope}`);
       }).catch(function(error) {
-        console.log(`Service worker failed to register: ${error}`);
+        // console.log(`Service worker failed to register: ${error}`);
       })
   });
 }
